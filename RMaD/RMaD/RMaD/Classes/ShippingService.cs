e88e658a61e5d3419bb80d87f701cef898bd6f6f
sshankargit <sshankar.sampath@gmail.com>
2022-12-03 21:02:26 -0800

    Added shipment classes

---
+++ RMaD/RMaD/RMaD/Classes/ShippingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMaD.Classes
+{
+    internal class ShippingService
+    {
+        private static SQLiteDataReader result;
+        private static SQLiteCommand sqlCommand;
+        static string sqlQuery;
+
+        public List<string> loadShippingServList()
+        {
+            List<string> shippingServList = new List<string>();
+
+            DatabaseAccess databaseObject = new DatabaseAccess();
+            sqlQuery = "SELECT SHIPPING_COMPANY_Name from SHIPPING_COMPANY";
+            sqlCommand = new SQLiteCommand(sqlQuery, databaseObject.sqlConnection);
+            //sqlCommand.Parameters.AddWithValue("@projectVer", projectVer);
+
+            databaseObject.OpenConnection();
+            result = sqlCommand.ExecuteReader();
+
+            //int moduleID = 0;
+
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    shippingServList.Add(result[0].ToString());
+                }
+            }
+
+            result.Close();
+            databaseObject.CloseConnection();
+
+            return shippingServList;
+        }
+
+    }
+}
