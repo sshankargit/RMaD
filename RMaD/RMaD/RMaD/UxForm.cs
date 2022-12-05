331dd47acc5abce05ce66f27df98586baeb38077
sshankargit <sshankar.sampath@gmail.com>
2022-12-04 16:18:36 -0800

    Database and UI changes done

--- RMaD/RMaD/RMaD/UxForm.cs
+++ RMaD/RMaD/RMaD/UxForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,10 @@ namespace RMaD
         List<Panel> panelList = new List<Panel>();
         List<Button> buttonList = new List<Button>();
         private int _shipments;
+
+        private SQLiteDataReader result;
+        private SQLiteCommand sqlCommand;
+        string sqlQuery;
 
         /// <summary>
         /// Counts the amount of shipments in the Flow Layout Panel 
@@ -43,8 +48,9 @@ namespace RMaD
             buttonList.Add(btnUserMenu);
             buttonList.Add(btnSettingsMenu);
 
-            this._shipments = this.flpShipments.Controls.Count;
+            //this._shipments = this.flpShipments.Controls.Count;
             changePanel(0);
+            populateDataGridView();
         }
 
         private void btnShipmentMenu_Click(object sender, EventArgs e)
@@ -104,7 +110,7 @@ namespace RMaD
                 if(drShip == DialogResult.OK)
                 {
                     this.Shipments++;
-                    newShipment(Shipments);
+                    //newShipment(Shipments);
                     creating = false;
                 }
                 else if(drShip == DialogResult.Cancel)
@@ -133,7 +139,43 @@ namespace RMaD
             btnShipment.TabIndex = 1;
             btnShipment.Text = "Shipment " + shipNum.ToString(); // change to data pull from data tables
             btnShipment.UseVisualStyleBackColor = true;
-            flpShipments.Controls.Add(btnShipment);
+            //flpShipments.Controls.Add(btnShipment);
         }
+
+        /// <summary>
+        /// Populate grid with shipments queried from database
+        /// </summary>
+        /// <param </param>
+        private void populateDataGridView()
+        {
+            dataGridViewShipment.DataSource = null;
+            DatabaseAccess databaseObject = new DatabaseAccess();
+            databaseObject.OpenConnection();
+            DataTable dt = new DataTable();
+
+            sqlQuery = "select S.tracking_id as [Tracking], S.shipped_on as [Shipped Date], S.arrive_on as [Arrival Date], SC.shipping_company_name as [Carrier], SS.status as Status " +
+                        "from SHIPMENT S " +
+                        "INNER JOIN SHIPPING_COMPANY SC on S.shipping_company_id = SC.shipping_company_id " +
+                        "INNER JOIN SHIPMENT_STATUS SS on S.shipment_status_id = SS.shipment_status_id " +
+                        "order by S.shipment_id DESC";            
+
+            SQLiteDataAdapter sqdt = new SQLiteDataAdapter(sqlQuery, databaseObject.sqlConnection);
+            sqdt.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                dataGridViewShipment.DataSource = dt;                
+
+                for (int i = 0; i < dataGridViewShipment.Columns.Count; i++)
+                {
+                    dataGridViewShipment.Columns[i].Frozen = false;
+                }
+
+                dataGridViewShipment.Columns["Tracking"].DefaultCellStyle.Format = "N2";
+                dataGridViewShipment.RowHeadersVisible = false;               
+            }            
+            databaseObject.CloseConnection();
+        }
+
     }
 }
