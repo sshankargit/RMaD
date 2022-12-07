97caef9d9c265e9d3645fb7b95db7c68dc7ffaeb
jdarts27 <joshua.dardis@trojans.dsu.edu>
2022-12-07 16:02:21 -0600

    Added accessability changes to add shipment.

--- RMaD/RMaD/RMaD/AddShipment.Designer.cs
+++ RMaD/RMaD/RMaD/AddShipment.Designer.cs
@@ -45,10 +45,9 @@
             // 
             this.lblTracking.AutoSize = true;
             this.lblTracking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTracking.Location = new System.Drawing.Point(116, 235);
-            this.lblTracking.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTracking.Location = new System.Drawing.Point(58, 122);
             this.lblTracking.Name = "lblTracking";
-            this.lblTracking.Size = new System.Drawing.Size(283, 37);
+            this.lblTracking.Size = new System.Drawing.Size(137, 20);
             this.lblTracking.TabIndex = 0;
             this.lblTracking.Text = "Tracking Number: ";
             // 
@@ -56,10 +55,9 @@
             // 
             this.lblCarrier.AutoSize = true;
             this.lblCarrier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCarrier.Location = new System.Drawing.Point(116, 437);
-            this.lblCarrier.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCarrier.Location = new System.Drawing.Point(58, 227);
             this.lblCarrier.Name = "lblCarrier";
-            this.lblCarrier.Size = new System.Drawing.Size(133, 37);
+            this.lblCarrier.Size = new System.Drawing.Size(64, 20);
             this.lblCarrier.TabIndex = 2;
             this.lblCarrier.Text = "Carrier: ";
             // 
@@ -67,10 +65,9 @@
             // 
             this.lblArrival.AutoSize = true;
             this.lblArrival.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArrival.Location = new System.Drawing.Point(116, 369);
-            this.lblArrival.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblArrival.Location = new System.Drawing.Point(58, 192);
             this.lblArrival.Name = "lblArrival";
-            this.lblArrival.Size = new System.Drawing.Size(238, 37);
+            this.lblArrival.Size = new System.Drawing.Size(117, 20);
             this.lblArrival.TabIndex = 3;
             this.lblArrival.Text = "Date of Arrival: ";
             // 
@@ -78,10 +75,9 @@
             // 
             this.lblShipped.AutoSize = true;
             this.lblShipped.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblShipped.Location = new System.Drawing.Point(116, 302);
-            this.lblShipped.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblShipped.Location = new System.Drawing.Point(58, 157);
             this.lblShipped.Name = "lblShipped";
-            this.lblShipped.Size = new System.Drawing.Size(228, 37);
+            this.lblShipped.Size = new System.Drawing.Size(115, 20);
             this.lblShipped.TabIndex = 4;
             this.lblShipped.Text = "Date Shipped: ";
             // 
@@ -89,10 +85,9 @@
             // 
             this.lblCreateShipment.AutoSize = true;
             this.lblCreateShipment.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCreateShipment.Location = new System.Drawing.Point(320, 108);
-            this.lblCreateShipment.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCreateShipment.Location = new System.Drawing.Point(160, 56);
             this.lblCreateShipment.Name = "lblCreateShipment";
-            this.lblCreateShipment.Size = new System.Drawing.Size(514, 73);
+            this.lblCreateShipment.Size = new System.Drawing.Size(256, 37);
             this.lblCreateShipment.TabIndex = 5;
             this.lblCreateShipment.Text = "Create Shipment";
             // 
@@ -100,22 +95,20 @@
             // 
             this.mtbTracking.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.mtbTracking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtbTracking.Location = new System.Drawing.Point(462, 225);
-            this.mtbTracking.Margin = new System.Windows.Forms.Padding(6);
+            this.mtbTracking.Location = new System.Drawing.Point(231, 117);
             this.mtbTracking.Name = "mtbTracking";
-            this.mtbTracking.Size = new System.Drawing.Size(512, 44);
-            this.mtbTracking.TabIndex = 6;
+            this.mtbTracking.Size = new System.Drawing.Size(257, 26);
+            this.mtbTracking.TabIndex = 0;
             this.mtbTracking.ValidatingType = typeof(int);
             // 
             // btnAddShipment
             // 
             this.btnAddShipment.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnAddShipment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddShipment.Location = new System.Drawing.Point(368, 569);
-            this.btnAddShipment.Margin = new System.Windows.Forms.Padding(6);
+            this.btnAddShipment.Location = new System.Drawing.Point(184, 296);
             this.btnAddShipment.Name = "btnAddShipment";
-            this.btnAddShipment.Size = new System.Drawing.Size(190, 63);
-            this.btnAddShipment.TabIndex = 7;
+            this.btnAddShipment.Size = new System.Drawing.Size(95, 33);
+            this.btnAddShipment.TabIndex = 4;
             this.btnAddShipment.Text = "Submit";
             this.btnAddShipment.UseVisualStyleBackColor = true;
             this.btnAddShipment.Click += new System.EventHandler(this.btnAddShipment_Click);
@@ -124,11 +117,10 @@
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(570, 569);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(6);
+            this.btnCancel.Location = new System.Drawing.Point(285, 296);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(190, 63);
-            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Size = new System.Drawing.Size(95, 33);
+            this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
@@ -136,35 +128,34 @@
             // 
             this.dtpShipped.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpShipped.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpShipped.Location = new System.Drawing.Point(462, 292);
-            this.dtpShipped.Margin = new System.Windows.Forms.Padding(6);
+            this.dtpShipped.Location = new System.Drawing.Point(231, 152);
             this.dtpShipped.Name = "dtpShipped";
-            this.dtpShipped.Size = new System.Drawing.Size(510, 44);
-            this.dtpShipped.TabIndex = 9;
+            this.dtpShipped.Size = new System.Drawing.Size(257, 26);
+            this.dtpShipped.TabIndex = 1;
             this.dtpShipped.ValueChanged += new System.EventHandler(this.dtpShipped_ValueChanged);
             // 
             // dtpArrival
             // 
             this.dtpArrival.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpArrival.Location = new System.Drawing.Point(462, 360);
-            this.dtpArrival.Margin = new System.Windows.Forms.Padding(6);
+            this.dtpArrival.Location = new System.Drawing.Point(231, 187);
             this.dtpArrival.Name = "dtpArrival";
-            this.dtpArrival.Size = new System.Drawing.Size(510, 44);
-            this.dtpArrival.TabIndex = 10;
+            this.dtpArrival.Size = new System.Drawing.Size(257, 26);
+            this.dtpArrival.TabIndex = 2;
             // 
             // tbCarrierdpdn
             // 
             this.tbCarrierdpdn.FormattingEnabled = true;
-            this.tbCarrierdpdn.Location = new System.Drawing.Point(462, 437);
+            this.tbCarrierdpdn.Location = new System.Drawing.Point(231, 227);
+            this.tbCarrierdpdn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbCarrierdpdn.Name = "tbCarrierdpdn";
-            this.tbCarrierdpdn.Size = new System.Drawing.Size(510, 33);
-            this.tbCarrierdpdn.TabIndex = 12;
+            this.tbCarrierdpdn.Size = new System.Drawing.Size(257, 21);
+            this.tbCarrierdpdn.TabIndex = 3;
             // 
             // AddShipment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1160, 806);
+            this.ClientSize = new System.Drawing.Size(580, 419);
             this.Controls.Add(this.tbCarrierdpdn);
             this.Controls.Add(this.dtpArrival);
             this.Controls.Add(this.dtpShipped);
@@ -177,7 +168,6 @@
             this.Controls.Add(this.lblCarrier);
             this.Controls.Add(this.lblTracking);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "AddShipment";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "New Shipment";
