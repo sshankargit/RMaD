28565ac894b8b4fba5d0a14fbfec2a3296e3e1cf
sshankargit <sshankar.sampath@gmail.com>
2022-12-08 20:22:17 -0800

    Email and User UI changes

--- RMaD/RMaD/RMaD/Classes/User.cs
+++ RMaD/RMaD/RMaD/Classes/User.cs
@@ -23,11 +23,11 @@ namespace RMaD.Classes
         private static SQLiteCommand sqlCommand;
         static string sqlQuery;
 
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
+        public string FirstName() { return this._firstname;}
+        public string Lastname() { return this._lastname; }
+        public string UserName() { return this._username; }
+        public string Password() { return this._password; }
+        public string Email() { return this._email; }
 
         public User(string firstname, string lastname, string username, string password, string email)
         {
@@ -42,6 +42,14 @@ namespace RMaD.Classes
         {
             this._username = username;
             this._password = password;
+        }
+
+        public User(string username)
+        {
+            this._username = username;
+            this._firstname = getUserFname();
+            this._lastname = getUserLname();
+            this._email = getUserEmailID();
         }
 
         public Boolean addUser()
@@ -108,6 +116,102 @@ namespace RMaD.Classes
             }
 
             return usrExists;
+        }
+
+        private string getUserEmailID()
+        {
+            DatabaseAccess databaseObject = new DatabaseAccess();
+            sqlQuery = "select email_address from USERS where user_name = @user";
+            sqlCommand = new SQLiteCommand(sqlQuery, databaseObject.sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@user", this._username);
+            databaseObject.OpenConnection();
+            result = sqlCommand.ExecuteReader();
+
+            string email = null;
+
+            if (result.HasRows)
+            {
+                if (result.Read())
+                {
+                    if (result[0].ToString() == string.Empty)
+                    {
+                        result.Close();
+                        databaseObject.CloseConnection();
+                        return null;
+                    }
+
+                    email = result[0].ToString();
+                }
+            }
+
+            result.Close();
+            databaseObject.CloseConnection();
+
+            return email;
+        }
+
+        private string getUserFname()
+        {
+            DatabaseAccess databaseObject = new DatabaseAccess();
+            sqlQuery = "select first_name from USERS where user_name = @user";
+            sqlCommand = new SQLiteCommand(sqlQuery, databaseObject.sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@user", this._username);
+            databaseObject.OpenConnection();
+            result = sqlCommand.ExecuteReader();
+
+            string fname = null;
+
+            if (result.HasRows)
+            {
+                if (result.Read())
+                {
+                    if (result[0].ToString() == string.Empty)
+                    {
+                        result.Close();
+                        databaseObject.CloseConnection();
+                        return null;
+                    }
+
+                    fname = result[0].ToString();
+                }
+            }
+
+            result.Close();
+            databaseObject.CloseConnection();
+
+            return fname;
+        }
+
+        private string getUserLname()
+        {
+            DatabaseAccess databaseObject = new DatabaseAccess();
+            sqlQuery = "select last_name from USERS where user_name = @user";
+            sqlCommand = new SQLiteCommand(sqlQuery, databaseObject.sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@user", this._username);
+            databaseObject.OpenConnection();
+            result = sqlCommand.ExecuteReader();
+
+            string lname = null;
+
+            if (result.HasRows)
+            {
+                if (result.Read())
+                {
+                    if (result[0].ToString() == string.Empty)
+                    {
+                        result.Close();
+                        databaseObject.CloseConnection();
+                        return null;
+                    }
+
+                    lname = result[0].ToString();
+                }
+            }
+
+            result.Close();
+            databaseObject.CloseConnection();
+
+            return lname;
         }
 
         public void removeUser(User user)
